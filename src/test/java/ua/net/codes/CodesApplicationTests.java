ea1884dd1996e1cecedb780b6ceef22e06b36049
ruslan <panruslan@gmail.com>
2020-12-06 15:24:30 +0200

    ci: setup sonarqube

---
+++ src/test/java/ua/net/codes/CodesApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.net.codes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
